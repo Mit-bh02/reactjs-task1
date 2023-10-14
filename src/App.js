fe37fdee542d20b3ece8508382d94690fc1b8941
Mit0921 <meetbhardiwala@gmail.com>
2023-10-14 20:06:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { WordCounter } from './components/word_counter';
+
+function App() {
+  return (
+    <div>
+      <WordCounter/>
+    </div>
+  );
+}
+
+export default App;
